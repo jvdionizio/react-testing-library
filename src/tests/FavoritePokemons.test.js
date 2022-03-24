abf0b64613ff2b5a9b4d938d115e9628a08e7654
jvdionizio <dionizio145@gmail.com>
2022-03-24 20:23:16 -0300

    require 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente <FavoritePokemons.js />', () => {
+  it('Testando se a mensagem correta é exibida quando não há favoritos', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favoriteButton = screen.getByLabelText(/pokémon favoritado\?/i);
+    userEvent.click(favoriteButton);
+    history.push('/favorites');
+    const pokemonCard = screen.getByText(/pikachu/i);
+    expect(pokemonCard).toBeInTheDocument();
+  });
+});

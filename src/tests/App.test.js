31e9fcdf99a5f3f1642ea454d8d38a2e41bf9deb
jvdionizio <dionizio145@gmail.com>
2022-03-24 12:11:06 -0300

    require 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente <App.js />', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('link', { name: /Home/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /About/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /Favorite Pokémons/i })).toBeInTheDocument();
+  });
+
+  it('Testando o redirecionamento do link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Testando o redirecionamento do link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Testando o redirecionamento do link Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favoriteLink);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Testando o redirecionamento ao entrar uma URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    expect(screen.getByRole('heading', { name: /Page requested not found/i }))
+      .toBeInTheDocument();
+  });
+});
